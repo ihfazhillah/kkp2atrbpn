6567586422be6848f6a06a3feb851fa5c323150d
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-13 12:03:06 +0700

    init validate persil

--- automate/kkp2.py
+++ automate/kkp2.py
@@ -1,6 +1,6 @@
 import requests
 
-from automate.pages import LoginPageObject, PilihKantorPageObject
+from automate.pages import LoginPageObject, PilihKantorPageObject, DetilQueryPageObject
 
 
 class KKP:
@@ -15,6 +15,7 @@ class KKP:
         self.login()
 
     def login(self):
+        print("login...")
         login_page = LoginPageObject(self.session)
         login_page.set_username(username=self.username)
         login_page.set_password(self.password)
@@ -23,3 +24,46 @@ class KKP:
         pilih_kantor = PilihKantorPageObject(self.session)
         pilih_kantor.pilih(self.kantor)
         pilih_kantor.submit()
+
+    def validasi_persil(self, kecamatan: str, desa: str) -> list:
+        print("validasi...")
+        """
+        Return not valid bidang, with its info and reason why its not valid
+
+        :param kecamatan:
+        :param desa:
+        :return:
+        """
+        not_valid_list = []
+
+        detail_query = DetilQueryPageObject(self.session)
+        detail_query.set_kecamatan(kecamatan)
+        detail_query.set_desa(desa)
+
+        while True:
+            start = detail_query.start_result
+            length = detail_query.num_result
+
+            resp = detail_query.cari(start, length)
+
+            print(*resp["data"], sep="\n")
+
+            if start >= resp["recordsFiltered"]:
+                break
+
+
+        return not_valid_list
+
+    def _validate_individual_persil(self, persil: dict, detail_query: DetilQueryPageObject):
+        pid = persil["PersilId"]
+        gambar = persil["Gambar"] == "true"
+        validasi_geom = persil["ValidasiGeom"] == "true"
+
+        if gambar and not validasi_geom:
+            # validate here
+            resp = detail_query.validate_bidang(pid)
+            valid = resp["Status"]
+            message = resp["Message"]
+
+            if not valid:
+                pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from requests.sessions import Session
 
+from automate.kkp2 import KKP
 from automate.pages import DetilQueryPageObject, LoginPageObject, PilihKantorPageObject
 from environs import Env
 
@@ -9,35 +10,52 @@ def main():
     env = Env()
     env.read_env()
 
-    s: Session = requests.Session()
-
-    login = LoginPageObject(s)
-    login.set_username(env("KKP_USERNAME"))
-    login.set_password(env("KKP_PASSWORD"))
-    login.submit()
-
-    pilih_kantor = PilihKantorPageObject(s)
-    pilih_kantor.pilih(env("KKP_KANTOR"))
-    pilih_kantor.submit()
-
-    detil_query = DetilQueryPageObject(s)
-    detil_query.set_kecamatan("tebo tengah")
-    detil_query.set_desa("semabu")
-    count = 1
-    while True:
-        print(f"pencarian page {count} dengan 20 hasil")
-        start = detil_query.start_result
-        length = detil_query.num_result
-        resp = detil_query.cari(start, length)
-        print(resp)
-        if start >= resp["recordsFiltered"]:
-            break
-        count += 1
-        print("")
-        print("")
-        print("")
-
-    # print(pilih_kantor.latest_response.resp.content)
+    kkp = KKP(env("KKP_USERNAME"), env("KKP_PASSWORD"), env("KKP_KANTOR"))
+    # kkp.validasi_persil("tebo tengah", "semabu")
+    print("validasi")
+    url = "https://kkp2.atrbpn.go.id/peta/DataSpasial/ValidasiBidang"
+    data = {
+        "pid": "9FDCF2AA1CE996E3E0530C1D140AC479"
+    }
+    resp = kkp.session.post(url, data=data)
+    print(resp.content)
+    url = "https://kkp2.atrbpn.go.id/peta/DataSpasial/DetilInfoFromPP"
+    data = {
+        "featureId": "PersilBerdasarkanStatusPendaftaran.9FDCF2AA1CE996E3E0530C1D140AC479",
+        "page": 1
+    }
+    resp = kkp.session.post(url, data=data)
+    print(resp.text)
+    #
+    # s: Session = requests.Session()
+    #
+    # login = LoginPageObject(s)
+    # login.set_username(env("KKP_USERNAME"))
+    # login.set_password(env("KKP_PASSWORD"))
+    # login.submit()
+    #
+    # pilih_kantor = PilihKantorPageObject(s)
+    # pilih_kantor.pilih(env("KKP_KANTOR"))
+    # pilih_kantor.submit()
+    #
+    # detil_query = DetilQueryPageObject(s)
+    # detil_query.set_kecamatan("tebo tengah")
+    # detil_query.set_desa("semabu")
+    # count = 1
+    # while True:
+    #     print(f"pencarian page {count} dengan 20 hasil")
+    #     start = detil_query.start_result
+    #     length = detil_query.num_result
+    #     resp = detil_query.cari(start, length)
+    #     print(resp)
+    #     if start >= resp["recordsFiltered"]:
+    #         break
+    #     count += 1
+    #     print("")
+    #     print("")
+    #     print("")
+    #
+    # # print(pilih_kantor.latest_response.resp.content)
 
 
 if __name__ == "__main__":
