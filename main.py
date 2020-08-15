158f4b5ab4c4da6d76102dbefac9503e1008d259
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-15 20:37:07 +0700

    ubah flow untuk dapatkan validation data

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def main():
     env.read_env()
 
     kkp = KKP(env("KKP_USERNAME"), env("KKP_PASSWORD"), env("KKP_KANTOR"))
-    non_valid_list = kkp.validasi_persil("tebo tengah", "kandang")
+    non_valid_list = kkp.validasi_persil("tebo tengah", "semabu")
 
-    with open("hasil.kandang.json", "w") as hasil:
-        json.dump(non_valid_list, hasil, indent=4)
+    with open("hasil.semabu.json", "w") as hasil:
+        json.dump(non_valid_list.all_result, hasil, indent=4)
 
-    df = pandas.read_json("hasil.kandang.json")
-    df.to_csv("hasil.csv")
+    df = pandas.read_json("hasil.semabu.json")
+    df.to_csv("hasil.semabu.csv")
 
     print("wes bar...")
 
